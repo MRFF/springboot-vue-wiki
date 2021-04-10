e40287b9d3fe3abbdba7e94d5c09218cdadfce35
Yeff <xyf220@126.com>
2021-04-10 12:17:55 +0800

    2-8 添加自定义配置，并在Java类中使用自定义配置中的属性

--- src/main/java/com/yifei/wiki/controller/TestController.java
+++ src/main/java/com/yifei/wiki/controller/TestController.java
@@ -1,17 +1,23 @@
 package com.yifei.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController // @Conroller + @ResponseBody
 public class TestController {
+
+
+    @Value("${hello.prop: defaultVal}")
+    private String prop;
+
     /*
     @RequestMapper(path = "/hello", method = RequestMethod.GET)
     @GetMapping("/hello")
      */
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello, Spring!";
+        return "Hello, Spring!" + prop;
     }
 }
