e04d8529d452df2516b5bd7f9ac86201b4b9cd97
Yeff <xyf220@126.com>
2021-04-11 11:32:19 +0800

    4-5 集成ant design vue

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入ant design vue组件与必要的全局css
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(store).use(router).mount('#app')
+// 记得再app中使用引入的组件
+createApp(App).use(store).use(router).use(Antd).mount('#app')
